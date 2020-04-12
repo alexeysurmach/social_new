977740693407cc0e65c68869b91300ad5e139c10
alexeysurmach <flashhome1985@gmail.com>
2020-04-12 17:04:30 +0300

    little css

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Lexa
+                </div>
+                <div className={s.dialog}>
+                    Roma
+                </div>
+                <div className={s.dialog}>
+                    Nastya
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}> Hey </div>
+                <div className={s.dialog}> Hi </div>
+                <div className={s.dialog}> Yo </div>
+            </div>
         </div>
     )
 }
