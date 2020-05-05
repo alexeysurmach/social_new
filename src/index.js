4529d613b828fe111503f1a2a5e41c64dfe35c1e
alexeysurmach <flashhome1985@gmail.com>
2020-05-05 11:58:45 +0300

    FLUX

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import state, {subscribe} from "./redux/state";
+import store from "./redux/state";
 import React from 'react';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ReactDOM from 'react-dom'
-import {addPost, updateNewPostText} from "./redux/state";
 
-let rerenderEntireThree = (state) => {
+let _callSubscriber = (state) => {
     ReactDOM.render(
         <React.StrictMode>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+            <App state={state} addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}/>
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
 
-rerenderEntireThree(state);
-subscribe(rerenderEntireThree);
+_callSubscriber(store.getState());
+store.subscribe(_callSubscriber);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,50 +1,53 @@
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'hi', likesCount: 13},
+                {id: 2, message: 'Hello', likesCount: 21}
+            ],
+            newPostText: ''
+        },
 
-let rerenderEntireThree =()=>{
 
-}
+        dialogPage: {
+            dialogs: [
+                {id: 1, name: 'Lexa'},
+                {id: 2, name: 'Nastya'},
+                {id: 3, name: 'Roma'}
+            ],
+            messages: [
+                {id: 1, message: 'Hey'},
+                {id: 2, message: 'Hi'},
+                {id: 3, message: 'Yo'}
+            ]
+        }
 
-let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'hi', likesCount: 13},
-            {id: 2, message: 'Hello', likesCount: 21}
-        ],
-        newPostText: ''
     },
-
-
-    dialogPage: {
-        dialogs: [
-            {id: 1, name: 'Lexa'},
-            {id: 2, name: 'Nastya'},
-            {id: 3, name: 'Roma'}
-        ],
-        messages: [
-            {id: 1, message: 'Hey'},
-            {id: 2, message: 'Hi'},
-            {id: 3, message: 'Yo'}
-        ]
+    getState(){
+        return this._state
+    },
+    _callSubscriber() {
+        console.log('State changed')
+    },
+    addPost() {
+        let newPost = {
+            id: 3,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText (newText)  {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe (obsever) {
+        this._callSubscriber = obsever;
     }
-
 }
 
-export const addPost = () => {
-    let newPost = {
-        id: 3,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText= '';
-    rerenderEntireThree(state);
-};
 
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText=newText;
-    rerenderEntireThree(state);
-};
-
-export let subscribe = (obsever)=>{
-rerenderEntireThree = obsever;
-}
-export default state
+export default store;
+window.store = store;
